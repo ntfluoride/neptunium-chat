f5e663d7666d5354b6deb6f7f340321e81a34b83
Neptunium Fluoride <13461827+ntfluoride@users.noreply.github.com>
2019-12-07 00:04:52 -0800

    Theming Update (July 21st, 2019)
    Added themes (Default, Inverted, Light, Summer, and Fall) and sound. ">" added at the end of the username. Touched up a few other things.

--- ntch.lua
+++ ntch.lua
@@ -1,39 +1,87 @@
--- Neptunium Chat (build1) | Built on FlexChat
-
+-- Neptunium Chat (build2 | July 21st, 2019) | Built on FlexChat
+ 
 term = require("term")
 component = require("component")
+computer = require("computer")
 event = require("event")
+gpu = component.gpu
+os = require("os")
+w, h = gpu.getResolution()
 serialization = require("serialization")
 messages = {}
 buffer = ""
 invalidating = false
-
-print("Welcome to Neptunium Chat, built upon (and compatible with) FlexChat! \n")
-print("Please insert the port you'd like to communicate through:")
+ 
+term.clear()
+ 
+computer.beep(100, 0.25)
+computer.beep(150, 0.25)
+computer.beep(200, 0.5)
+ 
+print("Welcome to Neptunium Chat (Build 2), built upon (and compatible with) FlexChat! \n")
+print("Please select the port you'd like to communicate through:")
 port = tonumber(term.read())
-
+ 
 while port == nil do
-    print("I'm sorry, that's not a valid port.")
+    print("Unknown port number.")
+    computer.beep(1500, 0.5)
     port = tonumber(io.stdin:read())
 end
-
-print("Please insert your alias:")
+ 
+print("Please choose a theme for your client:")
+print("[D]efault, [I]nverted [L]ight, [S]ummer, [F]all")
+usertheme = io.read()
+ 
+if usertheme == "D" or usertheme == "d" then -- Select the default theme
+    gpu.setForeground(0xFFFFFF)
+    gpu.setBackground(0x000000)
+    gpu.fill(1, 1, w, h, " ")
+ 
+elseif usertheme == "I" or usertheme == "i" then -- Select the inverted theme (better than light *I* guess :P)
+    gpu.setForeground(0x000000)
+    gpu.setBackground(0xFFFFFF)
+    gpu.fill(1, 1, w, h, " ")
+ 
+elseif usertheme == "L" or usertheme == "l" then -- Select the light theme (freak)
+    gpu.setForeground(0x000000)
+    gpu.setBackground(0xC0C0C0)
+    gpu.fill(1, 1, w, h, " ")
+ 
+elseif usertheme == "S" or usertheme == "s" then -- Select the bright summer theme
+    gpu.setForeground(0x008000)
+    gpu.setBackground(0x0000FF)
+    gpu.fill(1, 1, w, h, " ")
+ 
+elseif usertheme == "F" or usertheme == "f" then -- Select the warm fall theme
+    gpu.setForeground(0x808080)
+    gpu.setBackground(0x654321)
+    gpu.fill(1, 1, w, h, " ")
+ 
+elseif usertheme == "DE" or usetheme == "de" then -- Select the debug theme (easter egg!)
+    gpu.setForeground(0x000000)
+    gpu.setBackground(0x008000)
+    gpu.fill(1, 1, w, h, " ")
+ 
+else
+   end
+ 
+print("Please insert an alias:")
 alias = term.read()
 alias = alias:gsub("\n", "")
-
+ 
 term.clear()
-
+ 
 width, height = term.getViewport()
 height = height + 1
-
+ 
 component.modem.open(port)
-
+ 
 function getLines(str)
         local lines = math.ceil(str:len()/width)
         if lines == 0 then lines = 1 end
         return lines
 end
-
+ 
 function invalidate()
     invalidating = true
     term.clear()
@@ -44,41 +92,41 @@ function invalidate()
         local lines = getLines(message.content)
         lineBudget = lineBudget - lines
         if lineBudget <= 0 then break end
-            
+           
         term.setCursor(1, lineBudget)
-        term.write(("[" .. message.name .. "] " .. message.content), true)
+        term.write(("[" .. message.name .. "]> " .. message.content), true)
     end
-
+ 
     term.setCursor(1, height-bufLines)
     term.write(buffer, true)
     invalidating = false
 end
-
+ 
 function spin()
     while invalidating do
         -- Spin until done invalidating
     end
 end
-
+ 
 function moveCursorBack()
     local x, y = term.getCursor()
     if x ~= 1 then
         term.setCursor(x - 2, y)
     end
-    invalidate()
+  invalidate()
 end
-
+ 
 function unknownEvent() end -- If event is not relevant, do nothing
-eventHandlers = setmetatable({}, {__index = function() return unknownEvent end})
-
+eventHandlers = setmetatable({}, {__index = function() return unknownEvent end })
+ 
 function eventHandlers.modem_message(_, _, _, _, message)
     spin()
     local msg = serialization.unserialize(message)
     msg.name = msg.name:gsub("\n", "")
     table.insert(messages, 1, msg) -- The first message is the latest
-    invalidate()
+  invalidate()
 end
-
+ 
 function eventHandlers.key_down(kbdAddr, char, code, pName)
     spin()
     if char == 13 then -- Enter
@@ -86,30 +134,30 @@ function eventHandlers.key_down(kbdAddr, char, code, pName)
         buffer=""
         eventHandlers["modem_message"](nil, nil, nil, nil, serializedMsg)
         component.modem.broadcast(port, serializedMsg)
-        return
+      return
     end
-    
+   
     if char == 127 then -- Backspace
         moveCursorBack()
         buffer = buffer:sub(1, -2)
         invalidate()
         return
     end
-
+ 
     if char >= 63232 and char <= 63235 then -- Arrow keys
         return -- They will crash otherwise
     end
-    
+   
     buffer = buffer .. string.char(char)
     invalidate()
 end
-
+ 
 function handleEvent(eventId, ...)
     if (eventId) then
         eventHandlers[eventId](...)
     end
 end
-
+ 
 while true do
     term.setCursorBlink(true)
     handleEvent(event.pull())
